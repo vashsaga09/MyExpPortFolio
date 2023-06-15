0cb21d21e4469572cd78121c3bd8a1a242b52202
vashsaga09 <louienikko.go21@gmail.com>
2023-06-14 21:47:48 -0400

    project

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'IBM' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+/* GET project page. */
+router.get('/project', function(req, res, next) {
+  res.render('index', { title: 'Project' });
+});
+
+/* GET service page. */
+router.get('/service', function(req, res, next) {
+  res.render('index', { title: 'Service' });
+});
+
+module.exports = router;
